365e712d244ac17f49919f47d6eb3b5be41120a7
Victor Eduardo Bazterra <bazterra@gmail.com>
2019-10-16 15:17:52 -0400

    Updating tag to 0.5.0

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Victor E. Bazterra'
 author = 'Victor E. Bazterra'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.5.0'
 
 
 # -- General configuration ---------------------------------------------------
